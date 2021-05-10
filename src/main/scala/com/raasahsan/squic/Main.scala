b60b3eb1f4022d3262362594b979bbc9519339c9
Raas Ahsan <raas.ahsan@gmail.com>
2021-05-10 02:25:12 -0500

    Set up SSL context and SSL engine for TLSv1.3

--- src/main/scala/com/raasahsan/squic/Main.scala
+++ src/main/scala/com/raasahsan/squic/Main.scala
@@ -4,11 +4,37 @@ import cats.effect.{ExitCode, IO, IOApp}
 import fs2.io.net.Network
 import cats.effect.kernel.Resource
 import com.comcast.ip4s.Port
+import javax.net.ssl.{SSLContext, SSLEngine, SSLParameters, X509TrustManager}
+import java.security.cert.X509Certificate
 
 object Main extends IOApp {
+  def getSslContext: IO[SSLContext] =
+    IO {
+      val context = SSLContext.getInstance("TLSv1.3")
+      val trustManager = new X509TrustManager {
+        def checkClientTrusted(x: Array[X509Certificate], y: String): Unit = {}
+        def checkServerTrusted(x: Array[X509Certificate], y: String): Unit = {}
+        def getAcceptedIssuers(): Array[X509Certificate] = Array()
+      }
+      context.init(null, Array(trustManager), null)
+      context
+    }
+
+  def createSslEngine(sslContext: SSLContext): Resource[IO, SSLEngine] =
+    Resource.eval(
+      IO {
+        val engine = sslContext.createSSLEngine()
+        engine.setUseClientMode(false)
+        engine.setSSLParameters(new SSLParameters())
+        engine
+      }
+    )
+
   def app: Resource[IO, Unit] = {
     val network = Network.forAsync[IO]
     for {
+      sslContext <- Resource.eval(getSslContext)
+      sslEngine <- createSslEngine(sslContext)
       socketGroup <- network.datagramSocketGroup()
       socket <- socketGroup.openDatagramSocket(port = Some(Port.fromInt(8080).get))
       _ <- socket.reads.evalMap(datagram => IO.println(datagram.bytes.toArray.toList)).compile.drain.background
